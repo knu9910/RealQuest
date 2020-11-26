41f1dda621943c33cf332f59d26369817ac79220
knu9910 <knu9910@naver.com>
2020-11-26 18:55:04 +0900

    Set client Eslint, Typescript, Expo-cli
     - eslint set up
     - prettier set up
     - typescript set up
     - Expo-cli set up

---
+++ client/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    'env':    {
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended', 
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'project': './tsconfig.json',
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'react-hooks',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2,
+            { SwitchCase:1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        // 'no-empty-function': 'off',
+        // '@typescript-eslint/no-empty-function': 'off',
+        // '@typescript-eslint/no-empty-function': 'off',
+        // 'react/display-name': 'off',
+        // 'react/prop-types': 'off',
+    },
+    'settings': {
+        'react': {
+          'version': 'detect',
+        }
+    }
+};
